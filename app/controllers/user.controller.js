62df27b43fac995bf48d2dc28956aba67e9ad623
vranckxrobin <r0703028@student.thomasmore.be>
2021-01-28 14:28:28 +0100

    update permision fix

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -450,12 +450,16 @@ exports.update = async (req, res) => {
       console.log(permissionsRequest);
       console.log("update3");
 
+      let alternatif;
       for (let i = 0; i < permissionsRequest.length; i++) {
         if (authUserPermission.indexOf(permissionsRequest[i]) == -1) {
-          let alternatif = permissionsRequest[i].replace("_COMPANY", "");
+          alternatif = permissionsRequest[i].replace("_OWN", "");
           if (authUserPermission.indexOf(alternatif) == -1) {
-            ToHighPermissions = true;
-            i = permissionsRequest.length;
+            alternatif = alternatif.replace("_COMPANY", "");
+            if (authUserPermission.indexOf(alternatif) == -1) {
+              ToHighPermissions = true;
+              i = permissionsRequest.length;
+            }
           }
         }
       }
